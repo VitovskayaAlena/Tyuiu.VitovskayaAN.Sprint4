3a1dca3d066067b3ad5f75dbe16a9d6cdb9d701e
qqbelll <alenkav07@listru>
2025-11-09 17:06:35 +0500

    Task4

---
+++ Tyuiu.VitovskayaAN.Sprint4.Task4.V10/Program.cs
@@ -0,0 +1,61 @@
+﻿using Tyuiu.VitovskayaAN.Sprint4.Task4.V10.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнила: Витовская А. Н. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                            *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #10                                                             *");
+        Console.WriteLine("* Выполнила: Витовская Алёна Николаевна | РППб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Заменить нечетные элементы массива на 0                                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.Write("Введите кол-во строк в массиве: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите кол-во строк в массиве: ");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        int[,] array = new int[rows, columns];
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"Введите {i},{j} элемент массива: ");
+                array[i,j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("\nМассив:");
+        for (int i = 0; i < rows; i++) 
+        {
+            for (int j = 0; j < columns; j++) 
+            {
+                Console.Write($"{array[i,j]} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int[,] res = ds.Calculate(array);
+
+        for (int i = 0; i < res.GetLength(0); i++)
+        {
+            for (int j = 0; j < res.GetLength(1); j++)
+            {
+                Console.Write(res[i, j] + "       ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
